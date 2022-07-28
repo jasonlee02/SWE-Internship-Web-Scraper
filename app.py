edf39e1a1ee964eec22eca9ff8d22efcf13c74f8
Jason <jasonedminlee@gmail.com>
2022-07-27 23:22:37 -0500

    setup database

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class User(db.model):
+    id = db.Column(db.String)
+    locationX = db.Column(db.Integer)
+    locationY = db.Column(db.Integer)
 
 @app.route('/')
 def index():
