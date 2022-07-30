f9d6d2fe09915b293752ee449aaa4aa40e93b845
Jason <jasonedminlee@gmail.com>
2022-07-29 23:11:30 -0500

    start html

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class User(db.model):
-    id = db.Column(db.String)
-    locationX = db.Column(db.Integer)
-    locationY = db.Column(db.Integer)
+#class myInternships(db.Model):
+    #id = db.Column(db.Integer)
+    #locationX = db.Column(db.Integer)
+    #locationY = db.Column(db.Integer)
+
+#class generatedInternships(db.Model):
+    #id = db.Column(db.String)
+    #locationX = db.Column(db.Integer)
+    #locationY = db.Column(db.Integer)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/generate')
+def generate():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
