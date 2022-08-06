d30458d9f82482f8f2f93e279650687ef1647fba
Jason <jasonedminlee@gmail.com>
2022-08-05 22:56:01 -0500

    use maps to choose your location

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,22 @@
 function initMap() {
-  let map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 40.12, lng: -96.81 },
+    zoom: 5
   });
+
+  const marker = new google.maps.Marker({
+    map: map
+  });
+
+  const xinput = document.getElementById("xinput");
+  const yinput = document.getElementById("yinput");
+
+  map.addListener("click", (e) => {
+    map.panTo(e.latLng);
+    marker.setPosition(e.latLng);
+    xinput.value = e.latLng.lat();
+    yinput.value = e.latLng.lng();
+  })
 }
 
 window.initMap = initMap;
