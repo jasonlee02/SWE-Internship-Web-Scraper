4741c5941a002cb6bf67553bce6cc9421b6fdff9
Jason <jasonedminlee@gmail.com>
2022-08-15 21:15:22 -0500

    work on scraper

--- classes/job.py
+++ classes/job.py
@@ -1,6 +1,7 @@
 class job:
-    def __init__(self, company, location, link, competitiveness):
+    def __init__(self, company, name, location, link, competitiveness):
         self.company = company
+        self.name = name
         self.location = location
         self.link = link
         self.competitiveness = competitiveness
--- scrapers/LinkedIn.py
+++ scrapers/LinkedIn.py
@@ -19,15 +19,39 @@ class LinkedInScraper:
         requeststr = f"https://www.linkedin.com/jobs/search?distance=100&keywords={urlSearch}&location={self.location}"
         self.driver.get(requeststr)
 
+        self.hashStrings = set(())
+        self.jobs = []
+
     def getHTML(self):
         return self.driver.page_source
     
     def goNextPage(self):
-        elements = self.driver.find_elements(By.CLASS_NAME, "artdeco-pagination__indicator artdeco-pagination__indicator--number active selected ember-view")
+        elements = self.driver.find_elements(By.css(".artdeco-pagination__indicator .artdeco-pagination__indicator--number .active .selected .ember-view"))
         if len(elements) > 1:
             elements[1].click()
     
     def scrapePage(self):
+        jobLinks = []
         html = self.getHTML()
         soup = BeautifulSoup(html, 'lxml')
+        for jobCard in soup.select(".ember-view .jobs-search-results__list-item .occludable-update .p0 .relative .scaffold-layout__list-item"):
+            companyName = None
+            jobTitle = None
+
+            jobTitleTag = jobCard.select(".disabled .ember-view .job-card-container__link .job-card-list__title")
+            if jobTitleTag is not None:
+                jobTitle = jobTitleTag.string.strip()
+
+            companyNameTag = jobCard.select(".job-card-container__link .job-card-container__company-name .ember-view")
+            if companyNameTag is not None:
+                companyName = companyNameTag.string.strip()
+            
+            if companyName and jobTitle:
+                hashString = companyName + "_" + jobTitle
+                if hashString in self.hashStrings:
+                    pass
+                else:
+                    self.hashStrings.add(hashString)
+                    jobPostDetails = jobCard.select(".disabled .ember-view .job-card-container__link")
+                    jobPostLink = jobPostDetails.get("href")
     
